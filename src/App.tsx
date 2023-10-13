6b2880fe0bdfa79e9f2468cda755df49b6af07e4
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-10-13 17:55:59 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home.js";
+
+function App() {
+  return (
+    <div className="App" style={{ width: "100vw", height: "100vh" }}>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,55 @@
+// Scene.tsx
+import React, { useRef, useEffect } from "react";
+import "./styles/Home.css"; // Assuming you save your styles in Scene.css
+
+const Home: React.FC = () => {
+  const ballRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let mouseX = 0;
+    let mouseY = 0;
+    let ballX = 0;
+    let ballY = 0;
+    const speed = 0.02;
+
+    function animate() {
+      if (ballRef.current) {
+        const distX = mouseX - ballX;
+        const distY = mouseY - ballY;
+
+        ballX = ballX + distX * speed;
+        ballY = ballY + distY * speed;
+
+        ballRef.current.style.left = ballX + "px";
+        ballRef.current.style.top = ballY + "px";
+      }
+
+      requestAnimationFrame(animate);
+    }
+
+    const handleMouseMove = (event: MouseEvent) => {
+      mouseX = event.pageX;
+      mouseY = event.pageY;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    animate();
+
+    // Cleanup function to remove the event listener
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <section className="sceneSection">
+      <div className="text-content">
+        <h1>SEARCHING FOR LIGHT</h1>
+      </div>
+      <div className="ball" ref={ballRef}></div>
+    </section>
+  );
+};
+
+export default Home;
